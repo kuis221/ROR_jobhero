1ddaf7facff0ad04b2fadbac2cebf3b6423dfd0c
Simon <winewater301@gmail.com>
2016-09-22 07:57:13 +0200

    display tasks per category on homepage

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,7 +1,12 @@
 class VisitorsController < ApplicationController
 	def index
-		@tasks = Task.paginate(:page => params[:page], :per_page => 6)
     # @tasks = Task.page(params[:page]).order('created_at ASC')
     # @tasks = Task.paginate(:page => params[:page]).order('id DESC')
+    if params[:category].nil?
+      @tasks = Task.paginate(:page => params[:page], :per_page => 10)
+    else
+      @category = Category.find_by(:name => params[:category])
+      @tasks = @category.tasks.paginate(:page => params[:page], :per_page => 10)
+    end
 	end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160921050222) do
+ActiveRecord::Schema.define(version: 20160922021508) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20160921050222) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "description"
+    t.string   "image"
   end
 
   create_table "tasks", force: :cascade do |t|
